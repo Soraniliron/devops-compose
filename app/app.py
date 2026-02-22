236e797f4b0e93a48be70e81b6de722364aed1d1
Soraniliron <88350397+Soraniliron@users.noreply.github.com>
2026-02-22 19:43:20 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,17 +3,20 @@ import redis
 import os
 
 app = Flask(__name__)
-r = redis.Redis(
-    host=os.getenv("REDIS_HOST", "redis"),
-    port=6379,
-    decode_responses=True
-)
+
+# קריאת כתובת ה-Redis ממשתנה הסביבה שהגדרנו ב-Render
+redis_url = os.getenv("REDIS_URL", "redis://localhost:6379")
+
+# התחברות ל-Redis עם תמיכה בחיבור מאובטח (SSL)
+r = redis.from_url(redis_url, decode_responses=True, ssl_cert_reqs=None)
 
 @app.route("/")
 def hello():
-    count = r.incr("hits")
-    return f"Hello DevOps. Visits: {count}\n"
+    try:
+        count = r.incr("hits")
+        return f"Hello DevOps. Visits: {count}\n"
+    except Exception as e:
+        return f"Error connecting to Redis: {str(e)}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
